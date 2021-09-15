7923709e2d81d88d0b91dafc152e172d50991d7f
juuuujuuuuu <83672513+pernydev@users.noreply.github.com>
2021-09-15 22:28:46 +0300

    Server IDs

--- main.js
+++ main.js
@@ -15,20 +15,31 @@ console.log(SITE + 'Loading pages...');
 app.get('/', function (req, res) {
     res.render('frontend/pages/index', 
         { server: [
-            { name: 'Epic hub server', ramUsed: 10, ramAvl: 1024, cpuUsed: 2, cpuAvl: 100, diskUsed: 4024, diskAvl: 8024},
-            { name: 'Epic event server', ramUsed: 10, ramAvl: 2024, cpuUsed: 9, cpuAvl: 100, diskUsed: 4024, diskAvl: 8024},
-            { name: 'Super Epic minigame server', ramUsed: 103, ramAvl: 2024, cpuUsed: 9, cpuAvl: 100, diskUsed: 4024, diskAvl: 8024},
-            { name: 'Very epic survival server', ramUsed: 10, ramAvl: 6024, cpuUsed: 2, cpuAvl: 100, diskUsed: 4224, diskAvl: 8024}
+            { name: 'Epic hub server', ramUsed: 10, ramAvl: 1024, cpuUsed: 2, cpuAvl: 100, diskUsed: 4024, diskAvl: 8024, ID: "92jd3578"},
+            { name: 'Epic event server', ramUsed: 10, ramAvl: 2024, cpuUsed: 9, cpuAvl: 100, diskUsed: 4024, diskAvl: 8024, ID: "73id0563"},
+            { name: 'Super Epic minigame server', ramUsed: 103, ramAvl: 2024, cpuUsed: 9, cpuAvl: 100, diskUsed: 4024, diskAvl: 8024, ID: "63y5967"},
+            { name: 'Very epic survival server', ramUsed: 10, ramAvl: 6024, cpuUsed: 2, cpuAvl: 100, diskUsed: 4224, diskAvl: 8024, ID: "37hj9472   s"}
         ]
     });
 });
 
 app.get('/auth/login', function (req, res) {
-    res.render('frontend/pages/login');
+    res.render('frontend/pages/auth/login');
 });
 
 app.get('/auth/register', function (req, res) {
-    res.render('frontend/pages/register');
+    res.render('frontend/pages/auth/register');
+});
+
+app.get('/server/:serverID', function (req, res) {
+    res.render('frontend/pages/server/console', {
+        server: {
+            "ID": req.params.serverID,
+            "details": [
+                { name: 'Epic hub server', ramUsed: 10, ramAvl: 1024, cpuUsed: 2, cpuAvl: 100, diskUsed: 4024, diskAvl: 8024},
+            ]
+        }
+    });
 });
 
 console.log(SITE + 'Opening the website...');
